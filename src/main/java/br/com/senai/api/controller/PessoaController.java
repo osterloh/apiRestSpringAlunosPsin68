644e5c2816bc2c9e2f8377a31794efa3d4e2fea0
osterloh <johnatanosterloh@gmail.com>
2021-05-27 11:40:34 -0300

    Alterção do método de listar para o tipo de List<>

--- src/main/java/br/com/senai/api/controller/PessoaController.java
+++ src/main/java/br/com/senai/api/controller/PessoaController.java
@@ -1,13 +1,31 @@
 package br.com.senai.api.controller;
 
+import br.com.senai.domain.model.Pessoa;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class PessoaController {
 
     @GetMapping("/pessoas")
-    public String listar(){
-        return "Teste";
+    public List<Pessoa> listar(){
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.setId(1L);
+        pessoa1.setNome("Johnatan");
+        pessoa1.setEmail("johnatan@gmail.com");
+        pessoa1.setTelefone("(47)98472-7610");
+
+        Pessoa pessoa2 = new Pessoa(
+                2L,
+                "Mayara",
+                "mayara@gmail.com",
+                "(47)98877-5544"
+        );
+
+        return Arrays.asList(pessoa1, pessoa2);
     }
 }
