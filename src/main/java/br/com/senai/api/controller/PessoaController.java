cc3a312ab54ce182a278e37e129256a567cd7ff8
osterloh <johnatanosterloh@gmail.com>
2021-05-28 10:15:18 -0300

    Definição dos métodos de listagem

--- src/main/java/br/com/senai/api/controller/PessoaController.java
+++ src/main/java/br/com/senai/api/controller/PessoaController.java
@@ -1,31 +1,48 @@
 package br.com.senai.api.controller;
 
 import br.com.senai.domain.model.Pessoa;
+import br.com.senai.domain.repository.PessoaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+@AllArgsConstructor
 @RestController
+@RequestMapping("/pessoas")
 public class PessoaController {
 
-    @GetMapping("/pessoas")
+    private PessoaRepository pessoaRepository;
+
+    @GetMapping
     public List<Pessoa> listar(){
-        Pessoa pessoa1 = new Pessoa();
-        pessoa1.setId(1L);
-        pessoa1.setNome("Johnatan");
-        pessoa1.setEmail("johnatan@gmail.com");
-        pessoa1.setTelefone("(47)98472-7610");
+        return pessoaRepository.findAll();
+    }
 
-        Pessoa pessoa2 = new Pessoa(
-                2L,
-                "Mayara",
-                "mayara@gmail.com",
-                "(47)98877-5544"
-        );
+    @GetMapping("/nome/{pessoaNome}")
+    public List<Pessoa> listarPorNome(@PathVariable String pessoaNome){
+        return pessoaRepository.findByNome(pessoaNome);
+    }
 
-        return Arrays.asList(pessoa1, pessoa2);
+    @GetMapping("/nome/containing/{nomeContaining}")
+    public List<Pessoa> listarNomeContaining(@PathVariable String nomeContaining){
+        return pessoaRepository.findByNomeContaining(nomeContaining);
+    }
+
+    @GetMapping("{pessoaId}")
+    public ResponseEntity<Pessoa> buscar(@PathVariable Long pessoaId){
+        return pessoaRepository.findById(pessoaId)
+                .map(pessoa -> ResponseEntity.ok(pessoa))
+                .orElse(ResponseEntity.notFound().build());
     }
 }
