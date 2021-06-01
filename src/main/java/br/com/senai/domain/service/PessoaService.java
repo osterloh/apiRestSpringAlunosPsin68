956722b8c314abc5e0c4e5249cbdfecd068106c9
osterloh <johnatanosterloh@gmail.com>
2021-06-01 16:56:53 -0300

    Configuração da classe PessoaService

--- src/main/java/br/com/senai/domain/service/PessoaService.java
+++ src/main/java/br/com/senai/domain/service/PessoaService.java
@@ -1,2 +1,27 @@
-package br.com.senai.domain.service;public class PessoaService {
+package br.com.senai.domain.service;
+
+import br.com.senai.domain.exception.NegocioException;
+import br.com.senai.domain.model.Pessoa;
+import br.com.senai.domain.repository.PessoaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@AllArgsConstructor
+@Service
+public class PessoaService {
+
+    private PessoaRepository pessoaRepository;
+
+    @Transactional
+    public Pessoa cadastrar(Pessoa pessoa){
+        boolean emailValidation = pessoaRepository.findByEmail(pessoa.getEmail())
+                .isPresent();
+
+        if(emailValidation){
+            throw  new NegocioException("Já existe uma pessoa com este e-mail cadastrado.");
+        }
+
+        return pessoaRepository.save(pessoa);
+    }
 }
