3596c761a7d78b3e9802633afe65e82f8773413d
Johnatan Osterloh <johnatanosterloh@gmail.com>
2021-06-08 13:47:54 -0300

    Definição da classe SolicitacaoEntregaService

--- src/main/java/br/com/senai/domain/service/SolicitacaoEntregaService.java
+++ src/main/java/br/com/senai/domain/service/SolicitacaoEntregaService.java
@@ -1,2 +1,28 @@
-package br.com.senai.domain.service;public class SolicitacaoEntregaService {
+package br.com.senai.domain.service;
+
+import br.com.senai.domain.model.Entrega;
+import br.com.senai.domain.model.Pessoa;
+import br.com.senai.domain.model.StatusEntrega;
+import br.com.senai.domain.repository.EntregaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Service
+public class SolicitacaoEntregaService {
+
+	private PessoaService pessoaService;
+	private EntregaRepository entregaRepository;
+
+	public Entrega solicitar(Entrega entrega){
+		Pessoa pessoa = pessoaService.buscar(entrega.getPessoa().getId());
+		entrega.setPessoa(pessoa);
+
+		entrega.setStatus(StatusEntrega.PEDENTE);
+		entrega.setDataPedido(LocalDateTime.now());
+
+		return entregaRepository.save(entrega);
+	}
 }
