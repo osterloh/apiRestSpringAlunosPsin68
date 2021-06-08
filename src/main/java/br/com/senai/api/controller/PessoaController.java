92f729d0f29c7112b0f9c4774eb016a1729e55b2
Johnatan Osterloh <johnatanosterloh@gmail.com>
2021-06-08 13:44:41 -0300

    Refatoração dos métodos de cadastro e exclusão

--- src/main/java/br/com/senai/api/controller/PessoaController.java
+++ src/main/java/br/com/senai/api/controller/PessoaController.java
@@ -66,7 +66,7 @@ public class PessoaController {
             return ResponseEntity.notFound().build();
         }
 
-        pessoaRepository.deleteById(pessoaId);
+        pessoaService.excluir(pessoaId);
 
         return ResponseEntity.noContent().build();
     }
