b135bec7fa375f77930aefe6c0e851a511388f92
Johnatan Osterloh <johnatanosterloh@gmail.com>
2021-06-08 13:47:18 -0300

    Refatoração da classe PessoaService

--- src/main/java/br/com/senai/domain/service/PessoaService.java
+++ src/main/java/br/com/senai/domain/service/PessoaService.java
@@ -24,4 +24,14 @@ public class PessoaService {
 
         return pessoaRepository.save(pessoa);
     }
+
+    @Transactional
+    public void excluir(Long pessoaId){
+        pessoaRepository.deleteById(pessoaId);
+    }
+
+    public Pessoa buscar(Long pessoaId){
+        return pessoaRepository.findById(pessoaId)
+                .orElseThrow(() -> new NegocioException("Pessoa não encontrada."));
+    }
 }
