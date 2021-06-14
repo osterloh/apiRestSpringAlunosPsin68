b2b57293fae982b9f6de41bc176d2d730687a7a8
osterloh <johnatanosterloh@gmail.com>
2021-06-14 10:49:19 -0300

    Refatoração dos métodos para as novas regras de domínio e DTO

--- src/main/java/br/com/senai/api/controller/EntregaController.java
+++ src/main/java/br/com/senai/api/controller/EntregaController.java
@@ -1,10 +1,17 @@
 package br.com.senai.api.controller;
 
+import br.com.senai.api.assembler.EntregaAssembler;
+import br.com.senai.api.model.EntregaModel;
+import br.com.senai.api.model.input.EntregaInput;
 import br.com.senai.domain.model.Entrega;
 import br.com.senai.domain.service.SolicitacaoEntregaService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -12,10 +19,24 @@ import org.springframework.web.bind.annotation.*;
 public class EntregaController {
 
 	private SolicitacaoEntregaService solicitacaoEntregaService;
+	private EntregaAssembler entregaAssembler;
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Entrega solicitar(@RequestBody Entrega entrega){
-		return solicitacaoEntregaService.solicitar(entrega);
+	public EntregaModel solicitar(@Valid @RequestBody EntregaInput entregaInput){
+		Entrega novaEntrega = entregaAssembler.toEntity(entregaInput);
+		Entrega entrega = solicitacaoEntregaService.solicitar(novaEntrega);
+
+		return entregaAssembler.toModel(entrega);
+	}
+
+	@GetMapping
+	public List<EntregaModel> listar(){
+		return solicitacaoEntregaService.listar();
+	}
+
+	@GetMapping("/{entregaId}")
+	public ResponseEntity<EntregaModel> buscar(@PathVariable Long entregaId){
+		return solicitacaoEntregaService.buscar(entregaId);
 	}
 }
