7235b5f9b470ec222b2eefd3b4b75c4a4707065f
osterloh <johnatanosterloh@gmail.com>
2021-06-22 16:37:59 -0300

    Implementação do Spring security

--- src/main/java/br/com/senai/LogisticaApplication.java
+++ src/main/java/br/com/senai/LogisticaApplication.java
@@ -2,6 +2,7 @@ package br.com.senai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class LogisticaApplication {
